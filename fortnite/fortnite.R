0523131c253a1e29f4e438dab4cd6fbc5dde4c9e
Nishant Arora <nish54@gmail.com>
2018-02-18 20:24:38 -0500

    added database to hw2

--- fortnite/fortnite.R
+++ fortnite/fortnite.R
@@ -1,2 +1,8 @@
 # Fortnite Weapons Project
 
+library(tidyverse)
+
+weapons_tab <- read_csv("/Users/nishant/Desktop/data_science/fortnite/weapons.csv")
+View(weapons_tab)
+
+# plot dmg vs DPS to see what overall best weapon is, group by type
